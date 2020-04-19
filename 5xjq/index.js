ba68aaa2111b5ea61a09a4ade62bb85c7d90ebc5
Shuhan Hu <1119han@gmail.com>
2020-04-20 03:15:59 +0800

    JQuery demo

--- 5xjq/index.js
+++ 5xjq/index.js
@@ -16,8 +16,32 @@
 //   });
 // // });
 // $(document).ready(function () {
-document.addEventListener("DOMContentLoaded", function () {
-  document.querySelector("#btn").addEventListener("click", function () {
-    document.querySelector("h1").classList.toggle("large");
+// document.addEventListener("DOMContentLoaded", function () {
+//   document.querySelector("#btn").addEventListener("click", function () {
+//     document.querySelector("h1").classList.toggle("large");
+//   });
+// });
+
+function btnClickEvent(event) {
+  event.stopPropagation();
+  event.preventDefault();
+  $("h1").toggleClass("large");
+  console.log("btn clicked");
+}
+
+$(document).ready(function () {
+  $("#btn").on("click", btnClickEvent);
+  $(".wrapper").on("click", function () {
+    console.log("wrapper clicked");
+    $("#btn").off("click");
+  });
+  let counter = 0;
+  $("#addItem").on("click", function () {
+    $("#container").append(`<li class="item">Item${counter}</li>`);
+    counter++;
+  });
+  $("#container").on("click", ".item", function () {
+    // console.log("item clicked");
+    $(this).toggleClass("large");
   });
 });
